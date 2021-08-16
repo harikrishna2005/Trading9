bb96f7b977ce8eff703fc417453e3ffbf375fbad
hari krishna <harikrishna2005@gmail.com>
2021-08-16 15:46:43 +0530

    Add project files.

---
+++ Trading/BootStrapper.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using Prism.Ioc;
+using Prism.Mvvm;
+using Prism.Unity;
+
+namespace Trading
+{
+    class BootStrapper : PrismBootstrapper
+    {
+        
+
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+
+        protected override void InitializeShell(DependencyObject shell)
+        {
+            base.InitializeShell(shell);
+            App.Current.MainWindow = (Window)shell;
+            App.Current.MainWindow.Show();
+
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            //throw new System.NotImplementedException();
+            
+        }
+    }
+}
